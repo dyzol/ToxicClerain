80dc9b31fc90ab5ab2943dd21a194b0185f6fd84
porsce <swarovskiswanski@gmail.com>
2026-05-13 22:38:14 +1000

    javadocs for Damage

--- src/toxiccleanup/builder/Damage.java
+++ src/toxiccleanup/builder/Damage.java
@@ -2,21 +2,46 @@ package toxiccleanup.builder;
 
 import toxiccleanup.engine.game.Positionable;
 
+/**
+ * Represents damage that can be applied to machines and other game entities.
+ * A Damage object contains an x-y position and type identifier. The type
+ * allows different damage sources to be distinguished (eg. lightning vs acid damage)
+ * @invariant x and y always positive and (including 0)
+ * @invariant type never null
+ */
 public class Damage implements Positionable {
-    private int x = 0;
-    private int y = 0;
+    private int damageX;
+    private int damageY;
     public static final String UNTYPED = "untyped";
     private String type = UNTYPED;
 
+    /**
+     * Constructs a new Damage object at the given position.
+     * The damage type is initially set to {@value #UNTYPED}. Subclasses
+     * or callers may change it via {@link #setType(String)}.
+     * @requires position not null
+     * @param position the position where the damage occurs
+     */
     public Damage(Positionable position) {
-        this.x = position.getX();
-        this.y = position.getY();
+        this.damageX = position.getX();
+        this.damageY = position.getY();
     }
 
+    /**
+     * Sets the type of this damage.
+     * Damage types are used to identify the source of damage
+     * so that certain machines can be immune to specific damage types.
+     *
+     * @param value the damage type identifier
+     */
     public void setType(String value) {
         this.type = value;
     }
 
+    /** Returns the type of this damage.
+     * If not explicitly set, the type defaults to {@value #UNTYPED}.
+     * @return the damage type identifier
+     */
     public String getType() {
         return type;
     }
@@ -29,7 +54,7 @@ public class Damage implements Positionable {
      */
     @Override
     public int getX() {
-        return x;
+        return damageX;
     }
 
     /**
@@ -40,30 +65,30 @@ public class Damage implements Positionable {
      */
     @Override
     public int getY() {
-        return y;
+        return damageY;
     }
 
     /**
      * Set the horizontal (x-axis) coordinate of the component.
      *
-     * @param x The new horizontal coordinate for this component.
-     * @requires x >= 0
-     * @ensures getX() == x
+     * @param newDamageX The new horizontal coordinate for this component.
+     * @requires damageX >= 0
+     * @ensures getX() == damageX
      */
     @Override
-    public void setX(int x) {
-        this.x = x;
+    public void setX(int newDamageX) {
+        this.damageX = newDamageX;
     }
 
     /**
      * Set the vertical (y-axis) coordinate of the component.
      *
-     * @param y The new vertical coordinate for this component.
-     * @requires y >= 0
-     * @ensures getY() == y
+     * @param newDamageY the new vertical coordinate for this component.
+     * @requires damageY >= 0
+     * @ensures getY() == damageY
      */
     @Override
-    public void setY(int y) {
-        this.y = y;
+    public void setY(int newDamageY) {
+        this.damageY = newDamageY;
     }
 }
